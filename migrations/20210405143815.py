8c1867521f439a497f31783aaf5de850b5cda251
Francisco de Souza Júnior <fsjunior@gmail.com>
2021-04-05 12:03:49 -0300

    added a simple migration

---
+++ migrations/20210405143815.py
@@ -0,0 +1,13 @@
+"""
+Migration description here!
+"""
+name = '20210405143815'
+dependencies = []
+
+
+def upgrade(db: "pymongo.database.Database"):
+    pass
+
+
+def downgrade(db: "pymongo.database.Database"):
+    pass
